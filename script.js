38c8ad7343c714a0b2a3211ab283c7218a34fe10
Irin Akter <youremail@example.com>
2026-04-25 16:49:06 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+
+// Image day and Night part Start
+let myImage = document.getElementById("mainImage");
+
+
+function showDay() {
+    myImage.src = "day.png";
+}
+
+
+function showNight() {
+    myImage.src = "night.png";
+}
+
+// Image day and Night part End
+
+
+
+
+//Burger Part Start
+let burgerImage = document.getElementById("mainImage2");
+
+function showDay2() {
+
+    burgerImage.src = "images/patty2.png"; 
+}
+
+function showNight2() {
+    
+    burgerImage.src = "images/burgerban.png";
+}
+
+//Burger Part End
+
+
