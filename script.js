c077d7dc57404cbff6fd70268f34825424f7bd7d
Irin Akter <irinakter2926@gmail.com>
2026-08-23 00:54:37 +0600

    fix commit

--- script.js
+++ script.js
@@ -1,35 +1,38 @@
+const myImage = document.getElementById("mainImage");
+const burgerImage = document.getElementById("mainImage2");
 
-// Image day and Night part Start
-let myImage = document.getElementById("mainImage");
+const cityDayBtn = document.getElementById("cityDayBtn");
+const cityNightBtn = document.getElementById("cityNightBtn");
+const burgerPattyBtn = document.getElementById("burgerPattyBtn");
+const burgerBunBtn = document.getElementById("burgerBunBtn");
 
+function setActiveButton(activeButton, inactiveButtons) {
+    activeButton.classList.add("active");
+    inactiveButtons.forEach((button) => button.classList.remove("active"));
+}
 
 function showDay() {
+    if (!myImage) return;
     myImage.src = "day.png";
+    setActiveButton(cityDayBtn, [cityNightBtn]);
 }
-
 
 function showNight() {
+    if (!myImage) return;
     myImage.src = "night.png";
+    setActiveButton(cityNightBtn, [cityDayBtn]);
 }
 
-// Image day and Night part End
-
-
-
-
-//Burger Part Start
-let burgerImage = document.getElementById("mainImage2");
-
 function showDay2() {
-
-    burgerImage.src = "images/patty2.png"; 
+    if (!burgerImage) return;
+    burgerImage.src = "images/patty2.png";
+    setActiveButton(burgerPattyBtn, [burgerBunBtn]);
 }
 
 function showNight2() {
-    
+    if (!burgerImage) return;
     burgerImage.src = "images/burgerban.png";
+    setActiveButton(burgerBunBtn, [burgerPattyBtn]);
 }
-
-//Burger Part End
 
 
